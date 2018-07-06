c4d2635670b68b4f05ad931e7b46b7baee2cfe24
cryptobr <crypto-br@openmailbox.org>
2018-07-05 22:22:45 -0300

    Update check_speed_internet.sh

--- check_speed_internet.sh
+++ check_speed_internet.sh
@@ -1,4 +1,10 @@
 #!/bin/bash 
+
+#-------------------------------------------#
+#-- Check Speed Link Raspberry Pi ----------#
+#-- by: @cryptobr - on Telegram ------------#
+#-------------------------------------------#
+
 # Install speedtest-cli with 'apt-get install speedtest-cli -y'
 
 speedtest-cli > link.txt
