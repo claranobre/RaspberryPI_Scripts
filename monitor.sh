237286fcf38560d9162b861b8868ea960eaeb260
cryptobr <crypto-br@openmailbox.org>
2018-07-30 11:34:06 -0300

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+name=monitor
+
+if ! tmux has-session -t $name; then
+  tmux start \;\
+    new-session -d -s $name exit                                                  \;\
+    neww -n monitor 'speedometer -r eth0 -t eth0'                               \;\
+    splitw -v -p 60 'htop' \;\
+    splitw -v -p 45 'ping google.com'                                              \;\
+    splitw -h -p 60 'tshark -i wlan0 -f "src port 53" -n -T fields -e frame.time -e ip.src -e ip.dst -e dns.qry.name -e eth.dst'                                                         \;
+fi
+
+tmux attach -t monitor
