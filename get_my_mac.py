6cee6d92bfe2d5bc7897ae54b5caffed0e139a8e
cryptobr <crypto-br@openmailbox.org>
2018-08-23 14:00:21 -0700

    Update get_my_mac.py

--- get_my_mac.py
+++ get_my_mac.py
@@ -15,8 +15,38 @@ def get_my_mac():
     mac = os.system("arp -a | grep "+ip+" | cut -c22-38 > /home/pi/mac_list.txt")
     f = open("/home/pi/mac_list.txt", "r")
     return (f.read())
-              
+from flask import request
+from flask import jsonify
+from flask import Flask
+import os
+import requests
+from requests_jwt import JWTAuth
 
+
+app = Flask(__name__)
+
+@app.route("/get_my_mac", methods=["GET"])
+def get_my_mac():
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    mac = os.system("arp -a | grep "+ip+" | cut -c22-38 > /home/pi/mac_check.txt")
+    f = open("/home/pi/mac_check.txt", "r")
+    return (f.read())
+
+@app.route("/check_me", methods=["GET"])
+def check_me():
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    mac = os.system("arp -a | grep "+ip+" | cut -c22-38 > /home/pi/mac_check.txt")
+    auth = JWTAuth('YOur JWT')
+    f = open("/home/pi/mac_check.txt", "r")
+    out = requests.get("https://YOur_API_For_Check/"+str(mac), auth=auth)
+    if str(out).find('200') != -1:
+        return "MAC Free"
+    else:
+        os.system("cat /home/pi/mac_check.txt >> /home/pi/mac_block.txt")
+        os.system("sh /home/pi/scripts/block_mac.sh") # Download (https://raw.githubusercontent.com/crypto-br/RaspberryPI_Scripts/master/block_mac.sh)
+        return "MAC Block"
+    
+             
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0',port=5000)
